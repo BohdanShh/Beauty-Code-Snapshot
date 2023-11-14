e342538c9f2bcbebf7b3086ef8de2c65800ad284
BohdanSh <shemigonbogdanwork@gmail.com>
2023-11-14 18:27:58 +0200

    Added resizable buttons

--- src/features/CodeField/index.tsx
+++ src/features/CodeField/index.tsx
@@ -7,7 +7,9 @@ import Editor from 'src/features/Editor';
 
 import { useCodeField } from 'src/features/CodeField/useCodeField';
 import { ThemeClasses } from 'src/types';
-import { SearchParam, THEME_VALUES } from 'src/constants';
+import { THEME_VALUES } from 'src/constants';
+
+import styles from 'src/features/CodeField/styles.module.css';
 
 const CodeEditor: FC = () => {
   const { isDragging, userPreferences, handleResizeStart, handleResize, handleResizeStop } =
@@ -15,19 +17,45 @@ const CodeEditor: FC = () => {
 
   return (
     <Resizable
+      enable={{ left: true, right: true }}
+      resizeRatio={2}
+      handleStyles={{
+        left: {
+          left: '0',
+          zIndex: 10,
+          top: '50%',
+          width: '1.5rem',
+          height: '1.5rem',
+          transform: 'translate(-50%,-50%)',
+        },
+        right: {
+          right: '0',
+          zIndex: 10,
+          top: '50%',
+          width: '1.5rem',
+          height: '1.5rem',
+          transform: 'translate(50%,-50%)',
+        },
+      }}
+      handleClasses={{
+        left: styles.resizeBtn,
+        right: styles.resizeBtn,
+      }}
       defaultSize={{ width: userPreferences.width || 520, height: 380 }}
       minWidth={520}
       maxWidth={920}
+      minHeight={380}
       onResizeStart={handleResizeStart}
       onResize={handleResize}
       onResizeStop={handleResizeStop}
     >
-      <div className=" relative flex flex-col gap-3">
+      <div className="relative h-full flex flex-col gap-3">
         <div
-          className={`"h-[380px] ${THEME_VALUES?.[userPreferences.theme as keyof ThemeClasses]}`}
+          className={`h-full transition-all duration-200 ${
+            THEME_VALUES?.[userPreferences.theme as keyof ThemeClasses]
+          }`}
           style={{
             padding: userPreferences.padding,
-            resize: 'horizontal',
           }}
         >
           <Editor />
