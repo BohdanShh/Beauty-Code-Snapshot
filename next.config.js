31424deafd795d3685bb1d7714a0a388456c2b34
BohdanSh <shemigonbogdanwork@gmail.com>
2023-11-26 21:03:34 +0200

    Some fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'Beauty-Code-Snapshot';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}/`;
+
 const nextConfig = {
   output: 'export',
   distDir: 'dist',
+  basePath,
+  assetPrefix,
 };
 
 module.exports = nextConfig;
